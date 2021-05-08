35d6e21b0fff673f5d55f74bc36c47fc9764f4cb
sr-igs <80356835+sr-igs@users.noreply.github.com>
2021-05-08 17:07:01 +0100

    add API request from open weather API to get the forecast of next 7 days

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  const apiKey = "dde13eb089b8230fcc731404f219fa4d"; //apikey for openweather
+  let lat = 51.7527;
+  let lon =  -0.336;
+  const units = "metric";
+  const exclude = "minutely,hourly";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}`;
+  https.get(url,function(response){
+    console.log(response.statusCode);
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+    });
+
+  });
+})
+
 app.listen(port,function(){
   console.log(`App is running in port ${port}`)
 })
