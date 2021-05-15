188a1c085cfa5106138d13a460a1bf1542fa2c07
sr-igs <80356835+sr-igs@users.noreply.github.com>
2021-05-15 09:58:27 +0100

    Remove luxor module. WorkoutCalc module now working and receiving data correctly. Algorithm to process data missing

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const { DateTime } = require("luxon");
 const ejs = require("ejs");
-const workoutCalc = require(__dirname="/workoutCalc.js")
+const workoutCalc = require(__dirname+"/workoutCalc.js")
 
 const app = express();
 const port = 3000;
@@ -34,19 +33,6 @@ app.get("/results",function(req,res){
 })
 
 app.post("/results", function(req, res) {
-  // let outdoorDays = Number(req.body.outdoor); //Number of outdoor workout days from the request
-  // let indoorDays = Number(req.body.indoor); //Number of indoor workout days from the request
-  //
-  // //Need to add code to detect the checkboxes. Challenge is that if checkbox is not marked it won't submit a value to the server!!!
-  // let dayPreference = {monday:false,tuesday:false,wednesday:false,thursday:false,friday:false,saturday:false,sunday:false};
-  // if(typeof(req.body.mondayCB)!=="undefined"){dayPreference.monday=true};
-  // if(typeof(req.body.tuesdayCB)!=="undefined"){dayPreference.tuesday=true};
-  // if(typeof(req.body.wednesdayCB)!=="undefined"){dayPreference.wednesday=true};
-  // if(typeof(req.body.thursdayCB)!=="undefined"){dayPreference.thursday=true};
-  // if(typeof(req.body.fridayCB)!=="undefined"){dayPreference.friday=true};
-  // if(typeof(req.body.saturdayCB)!=="undefined"){dayPreference.saturday=true};
-  // if(typeof(req.body.sundayCB)!=="undefined"){dayPreference.sunday=true};
-  // console.log(dayPreference);
 
 //MUST REFACTOR THE BELOW!!!!
   const apiKey = "dde13eb089b8230fcc731404f219fa4d"; //apikey for openweatherv  - This will have to go in a private file that doesn't get uploaded to git
@@ -70,7 +56,7 @@ app.post("/results", function(req, res) {
       //console.log(weatherData);
       var hourlyData = weatherData.hourly;
       var dailyData = weatherData.daily;
-      doWeatherCalcs(dailyData,indoorDays,outdoorDays,dayPreference);
+      appResults = workoutCalc.doCalcs(req.body,dailyData);
       res.redirect("/results")
     });
   });
@@ -83,31 +69,3 @@ app.post("/", function(req, res) {
 app.listen(port, function() {
   console.log(`App is running in port ${port}`)
 })
-
-//Refactor the below too
-function doWeatherCalcs(data,indoor,outdoor,dayPreference) {
-  let weatherOutput={
-    description:"",
-  };
-  let dateStamps = [];
-  let weatherPoints = [];
-  for(var i=0;i<data.length-1;i++){ //Removing last day as it is repetition of current day
-    //dateStamps array will contain 1-7 indicating Monday to Sunday, uses Luxon for time stamp management.
-    dateStamps.push(DateTime.fromSeconds(Number(data[i].dt)).weekday);
-    //Gets the weather string
-    weatherPoints.push(data[i].weather[0].main);
-    //FUTURE: Get weather icon too!
-  };
-  console.log(dateStamps);
-  console.log(weatherPoints);
-  let rainDays = 0;
-  //Count how many rainy days - there probably is a better way of doing this.
-  weatherPoints.forEach(function(w){
-    if(w.includes("Rain")){rainDays++};
-  });
-
-  //1) In the days prefered, first add indoor workouts for any rainy days
-  //2) In the days prefered, then add outdoor workouts for any non-rainy days
-  //3) If all days have not been covered, then add other days randomly, prioritisign week days.
-
-};
--- workoutCalc.js
+++ workoutCalc.js
@@ -1,16 +1,106 @@
-function doCalcs(userReq){
+exports.doCalcs = function(userReq, weatherData) {
   //userReq will be the req.body
   let outdoorDays = Number(userReq.outdoor); //Number of outdoor workout days from the request
   let indoorDays = Number(userReq.indoor); //Number of indoor workout days from the request
+  console.log("Outdoor" + outdoorDays);
+  console.log("Indoor:" + indoorDays);
+
+  //This is the object that will hold the information that is passed to the website
+  let appResults = {
+    firstDay: ["Monday", "Outdoor"],
+    secondDay: ["Monday", "Indoor"],
+    thirdDay: ["Monday", "Outdoor"],
+    fourthDay: ["Monday", "Indoor"],
+    fifthDay: ["Monday", "Indoor"],
+    sixthDay: ["Monday", "Indoor"],
+    seventhDay: ["Monday", "Indoor"]
+  }
 
   //Need to add code to detect the checkboxes. Challenge is that if checkbox is not marked it won't submit a value to the server!!!
-  let dayPreference = {monday:false,tuesday:false,wednesday:false,thursday:false,friday:false,saturday:false,sunday:false};
-  if(typeof(userReq.mondayCB)!=="undefined"){dayPreference.monday=true};
-  if(typeof(userReq.tuesdayCB)!=="undefined"){dayPreference.tuesday=true};
-  if(typeof(userReq.wednesdayCB)!=="undefined"){dayPreference.wednesday=true};
-  if(typeof(userReq.thursdayCB)!=="undefined"){dayPreference.thursday=true};
-  if(typeof(userReq.fridayCB)!=="undefined"){dayPreference.friday=true};
-  if(typeof(userReq.saturdayCB)!=="undefined"){dayPreference.saturday=true};
-  if(typeof(userReq.sundayCB)!=="undefined"){dayPreference.sunday=true};
+  let dayPreference = {
+    monday: false,
+    tuesday: false,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+    sunday: false
+  };
+  if (typeof(userReq.mondayCB) !== "undefined") {
+    dayPreference.monday = true
+  };
+  if (typeof(userReq.tuesdayCB) !== "undefined") {
+    dayPreference.tuesday = true
+  };
+  if (typeof(userReq.wednesdayCB) !== "undefined") {
+    dayPreference.wednesday = true
+  };
+  if (typeof(userReq.thursdayCB) !== "undefined") {
+    dayPreference.thursday = true
+  };
+  if (typeof(userReq.fridayCB) !== "undefined") {
+    dayPreference.friday = true
+  };
+  if (typeof(userReq.saturdayCB) !== "undefined") {
+    dayPreference.saturday = true
+  };
+  if (typeof(userReq.sundayCB) !== "undefined") {
+    dayPreference.sunday = true
+  };
 
+  console.log(dayPreference);
+
+  let dateStamps = [];
+  let weatherPoints = [];
+  for (var i = 0; i < weatherData.length - 1; i++) {
+    let tempDate = new Date(0)
+    tempDate.setUTCSeconds(weatherData[i].dt);
+    dateStamps.push(tempDate.getDay());
+    weatherPoints.push(weatherData[i].weather[0].main)
+  };
+  console.log(dateStamps);
+  console.log(weatherPoints);
+
+  let rainDays = 0;
+  //Count how many rainy days - there probably is a better way of doing this.
+  weatherPoints.forEach(function(w) {
+    if (w.includes("Rain")) {
+      rainDays++
+    };
+  });
+
+  //I have all the data I needed - time for the algorithm!
+  //1) If rain days = 7, doesn't matter: what is the default order?
+  //2
+
+  return appResults
+}
+
+function dayToString(day) {
+  switch (day) {
+    case 0:
+      return "Sunday"
+      break;
+    case 1:
+      return "Monday"
+      break;
+    case 2:
+      return "Tuesday"
+      break;
+    case 3:
+      return "Wednesday"
+      break;
+    case 4:
+      return "Thursday"
+      break;
+    case 5:
+      return "Friday"
+      break;
+    case 6:
+      return "Saturday"
+      break;
+    default:
+      return "Error"
+  }
+}
 }
